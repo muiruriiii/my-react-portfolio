4e89a9f0092b06cfa05a9487a39afa4855173278
muiruriiii <muiruricynthiaaa@gmail.com>
2024-01-28 14:19:07 +0300

    feat: add services component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Header from "./components/Header/header";
 import About from "./components/About/about";
+import Service from "./components/Services/service";
 
 function App() {
   return (
     <div className="App">
         <Header/>
         <About/>
+        <Service/>
     </div>
   );
 }
---
+++ src/components/Services/service.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './service.css';
+
+const Service = () => {
+    return (
+        <section id="services">
+            <div className="container">
+                <h1 className="subtitle2">WHAT I OFFER</h1>
+
+            </div>
+        </section>
+    );
+}
+
+export default Service;
