3ed40ae0520aa764c9e2a2f8feb650a3fced6289
muiruriiii <muiruricynthiaaa@gmail.com>
2024-01-28 16:03:51 +0300

    feat(api): integrate chuck norris joke endpoint

--- src/components/Jokes/jokes.js
+++ src/components/Jokes/jokes.js
@@ -1,10 +1,23 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import './jokes.css';
+import axios from "axios";
 
 const Jokes = () => {
+
+    const [jokes, setJokes] = useState()
+
+    useEffect(()=>{
+        const fetchData = async() => {
+           const joke = await axios.get("https://api.chucknorris.io/jokes/random")
+            setJokes(joke.data.value)
+        }
+        fetchData()
+    }, []);
+
     return (
         <section id="jokes">
-
+            <h1>Joke of the day: </h1>
+            <p>{jokes}</p>
         </section>
     );
 }
