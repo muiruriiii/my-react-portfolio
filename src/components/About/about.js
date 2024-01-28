303abeda708626049d5e03c9e2c8b32989bc86f5
muiruriiii <muiruricynthiaaa@gmail.com>
2024-01-28 14:00:46 +0300

    feat: add about component

---
+++ src/components/About/about.js
@@ -0,0 +1,17 @@
+import React from "react";
+import './about.css';
+
+const About = () => {
+
+    return (
+        <section id="about">
+            <div className="container">
+                <div className="row">
+
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default About;
