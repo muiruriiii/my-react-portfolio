093610a9fd4e76efa41121b633c6efbe809c7a0b
muiruriiii <muiruricynthiaaa@gmail.com>
2024-01-28 14:06:27 +0300

    feat(elements): add about column layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Header from "./components/Header/header";
+import About from "./components/About/about";
 
 function App() {
   return (
     <div className="App">
         <Header/>
+        <About/>
     </div>
   );
 }
--- src/components/About/about.js
+++ src/components/About/about.js
@@ -1,5 +1,6 @@
 import React from "react";
 import './about.css';
+import about from '../../assets/about-background.jpeg';
 
 const About = () => {
 
@@ -7,7 +8,19 @@ const About = () => {
         <section id="about">
             <div className="container">
                 <div className="row">
+                    <div className="about-col-1">
+                        <img src={about} alt="About background" />
+                    </div>
+                    <div className="about-col-2">
+                        <h1 className="subtitle">About Me</h1>
+                        <p>
+                            I am a Computer Science finalist who has garnered software development skills and is very
+                            passionate about data.
+                            I possess problem-solving, leadership, interpersonal as well as great communication skills.
+                            I am self-driven and a team player that performs well in collaborative environments.
+                        </p>
 
+                    </div>
                 </div>
             </div>
         </section>
